f24cc8cc2ebf519c6bcd4c0ca758eb558d208f96
Buddz-CodeRed <badi9code.red@gmail.com>
2026-03-24 10:10:24 +1300

    test

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   base: '/COMP6210-Assignment-1',
-  plugins: [react()],
-  // allow all local devices access to server
-  server: {
-    host: true,
-    port: 5173,
-  }
+  plugins: [react()]
 })
