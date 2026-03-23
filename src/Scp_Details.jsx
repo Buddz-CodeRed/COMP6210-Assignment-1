3e939c70ed10d96cdb0092508b4d30f095e8e1a7
Buddz-CodeRed <badi9code.red@gmail.com>
2026-03-23 23:35:40 +1300

    restructured Scp_Details.jsx and refined index.css to be responsive on all devices portrait and landscape

--- src/Scp_Details.jsx
+++ src/Scp_Details.jsx
@@ -10,47 +10,50 @@ export default function Scp_Details()
     
     return(
         <div className='shadow_wrapper'>
-            <div className='header_wrapper'>
+            
+            {/* Header Section */}
+            <div className="header_container">
 
-                <h2 className='file_heading'>SCP Foundation</h2>
+                <div className="header_wrapper">
+                    <h2 className='file_heading'>SCP Foundation</h2>
 
-                <div className='stamp_logo'>
-                    <img src="/images/stamp-Photoroom.png" alt="Scp Logo" className='logo'/>
-                </div>
-                {/* access a specific property of the current array and display it */}
-                <div className='header_items'>
-                    <div className='sec_1'>
-                        <h3>Subject</h3>
-                        <h2> {currentObject.subject}</h2>
+                    <div className="stamp_logo">
+                        <img src="/images/stamp-Photoroom.png" alt="Scp Logo" className='logo'/>
                     </div>
-                    <div className='sec_2'>
-                        <h3>Class</h3>
-                        <h2>{currentObject.class}</h2>
-                    </div>                    
+
+                    <div className="header_items">
+                        <div className='sec_1'>
+                            <h3>Subject</h3>
+                            <h2>{currentObject.subject}</h2>
+                        </div>
+
+                        <div className="sec_2">
+                            <h3>Class</h3>
+                            <h2>{currentObject.class}</h2>
+                        </div>
+                    </div>           
                 </div>
-                {/* <img src={currentObject.icon} alt="Icon" className='currentObjectIcon'/>                 */}
+
+                <img src={currentObject.icon} alt="Subject Icon" className='currentObjectIcon' />
+
             </div>
-            <img src={currentObject.icon} alt="Icon" className='currentObjectIcon'/>
-            <div className="image_container">
-                <img src={currentObject.image} alt="SCP Image" className="scpImage"/>
-            </div>
-            <div className='glass_morph'>
-                <div className='desc'>
+            <div className="glass_morph">
+                <div className="desc">
                     <h2>Description</h2>
                     <p>{currentObject.description}</p>
                 </div>
-                <div className='contain'>
+
+                <div className="image_container">
+                    <img src={currentObject.image} alt="SCP Image" className="scpImage"/>
+                </div>
+
+                <div className="contain">
                     <h2>Containment Procedure</h2>
 
-                    {/* Confirms if the objects type is a string */}
                     {typeof currentObject.containment === 'string' ? (
-                        // Display string directly
-                        <p>{currentObject.containment}</p>
-                    ):(
-                        // extract all values from the object and convert is to an array
-                        // loop through each item in the array and use the placeholder (count(i)) to id each value
+                    <p>{currentObject.containment}</p>
+                    ) : (
                         Object.values(currentObject.containment).map((procedure, i) => (
-                            // display the current value of (i)
                             <p key={i}>{procedure}</p>
                         ))
                     )}
